b7b0acb33c2883b70f5ba9fbb3b3052bbd3a992f
MJ <mercyorangi2@gmail.com>
2023-10-24 10:47:04 -0400

    initial commit

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -10,4 +10,4 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://evemwangi:Nima900##@localhost:5432/fire'
