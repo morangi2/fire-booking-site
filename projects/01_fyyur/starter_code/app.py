a153f251f80c0d3390ba43177ad06277cac60a14
MJ <mercyorangi2@gmail.com>
2023-10-24 20:54:12 -0400

    create new show

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -617,11 +617,37 @@ def create_show_submission():
   # called to create new shows in the db, upon submitting new show listing form
   # TODO: insert form data as a new Show record in the db, instead
 
-  # on successful db insert, flash success
-  flash('Show was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Show could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+  error = False
+
+  try:
+    artist_id = request.form['artist_id']
+    venue_id = request.form['venue_id']
+    start_time = request.form['start_time']
+
+    show = Show(artist_id=artist_id, venue_id=venue_id, start_time=start_time)
+
+    db.session.add(show)
+    db.session.commit()
+
+    print(show)
+
+  except:
+    error = True
+    db.session.rollback()
+
+  finally:
+    db.session.close()
+
+  if error:
+    # TODO: on unsuccessful db insert, flash an error instead.
+    # e.g., flash('An error occurred. Show could not be listed.')
+    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+    flash('An error occured. SHOW could not be listed.')
+
+  else:
+    # on successful db insert, flash success
+    flash('Show was successfully listed!')
+
   return render_template('pages/home.html')
 
 
